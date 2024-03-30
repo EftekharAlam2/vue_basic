ed943d393696d16c53aa38591b987b082e6ddae8
EftekharAlam2 <hinosuvlmare@gmail.com>
2024-03-30 10:45:40 +0600

    v-if and v-for condition added

--- main.js
+++ main.js
@@ -11,3 +11,22 @@ var app2 = new Vue ({
         message: 'You load this pages on ' + new Date().toLocaleString()
     }
 })
+
+var app3 = new Vue ({
+    el: '#app3',
+    data:{
+        seen: true
+    }
+})
+
+var app4 = new Vue ({
+    el: '#app4',
+    data:{
+        todos: [
+            { text: 'Learn HTML' },
+            { text: 'Learn CSS' },
+            { text: 'Learn JavaScript' },
+            { text: 'Learn Vue js' }
+        ]
+    }
+})
