9db164053d06651189a31898d40e58bf7fc3dd74
EftekharAlam2 <hinosuvlmare@gmail.com>
2024-03-30 10:37:54 +0600

    v-bind attribute use

--- main.js
+++ main.js
@@ -4,3 +4,10 @@ var app = new Vue ({
         message: "Hello VUE!"
     }
 })
+
+var app2 = new Vue ({
+    el: '#app2',
+    data:{
+        message: 'You load this pages on ' + new Date().toLocaleString()
+    }
+})
